c50f29c8c4cbc3dcd9ec67a8d4328c465a3e6fbc
Guro Halvorsdatter Royneland <gurohr@icloud.com>
2026-08-11 15:15:18 +0200

    Feature: Add utils.py with functions to measure similarity for face embeddings

---
+++ utils.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def cosine_similarity(a: np.ndarray, b: np.ndarray) -> float:
+    """Cosine similarity between two L2-normalised vectors. Range [-1, 1]."""
+    return float(np.dot(a, b))
+
+def cosine_distance(a: np.ndarray, b: np.ndarray) -> float:
+    """Cosine distance. Range [0, 2]."""
+    return 1.0 - cosine_similarity(a, b)
